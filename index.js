101f27baf00748d22b1f920b0761d29cd3f8efa3
shafik720 <shafikrasel5@gmail.com>
2023-06-06 20:31:26 +0600

    trying middleware

--- index.js
+++ index.js
@@ -198,7 +198,7 @@ app.get('/', (req, res) => {
 
 // Enable CORS middleware
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000/');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();
